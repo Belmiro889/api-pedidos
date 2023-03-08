1433a761102cadd019675c87905e232a644aba8c
Belmiro Tobias Nascimento Placido <tobias.belmiro88@gmail.com>
2023-03-08 16:40:20 -0300

    Class Categoria criada

--- src/main/java/com/belmiro/pedidos/resources/CategoriasResources.java
+++ src/main/java/com/belmiro/pedidos/resources/CategoriasResources.java
@@ -1,15 +1,29 @@
 package com.belmiro.pedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.belmiro.pedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriasResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Funcionando";
+	public List<Categoria> listar() {
+
+		List<Categoria> list = new ArrayList<Categoria>();
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
+
 	}
 }
